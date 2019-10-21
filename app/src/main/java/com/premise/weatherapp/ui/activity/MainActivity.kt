1c37473e93bb01ac3c011c6a1d8d71c1c20c3f13
Joseph L. Malone <josephmalone@lululemon.com>
2019-10-20 22:51:36 -0400

    Story-1 Feature Complete for this Story As a user, I can enter my location into the app. There should be an input field allowing a user to enter a location (a City/state/country/zipcode/etc) into the app.
    Added Navigation Components and attacked it to the xml of the Main Activity as the first step in an MVVM architecture.
    
    The navigation graph is similar to a storyboard in iOS
    
    Created an InputFragment which is the first screen for the new navigation Graph

--- app/src/main/java/com/premise/weatherapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/premise/weatherapp/ui/activity/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.premise.weatherapp.ui.activity
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.premise.weatherapp.R
 
 class MainActivity :AppCompatActivity(){
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
