a8bf6d2c8683618b0374cd2e80e33be215856eaa
Joseph L. Malone <josephmalone@lululemon.com>
2019-10-21 02:44:42 -0400

    Story-3 Sadly All my tests are broken because of some espresso library conflicts Which I do not have any more time to solve
    InputScreenTest was added to explicitly test the fragment graph navigation component traversal

---
+++ app/src/androidTest/java/com/premise/weatherapp/ui/activity/InputScreenTest.kt
@@ -0,0 +1,42 @@
+package com.premise.weatherapp.ui.activity
+
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.premise.weatherapp.R
+import com.premise.weatherapp.ui.fragment.InputFragment
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+/**
+ * A simple test class that can be run both on device (or emulator) or on the host (as a JVM test
+ * using Robolectric).
+ */
+@RunWith(AndroidJUnit4::class)
+class InputScreenTest {
+
+    @Test
+    fun test1() {
+        // Create a mock NavController
+        val mockNavController = mock(NavController::class.java)
+
+        // Create a graphical FragmentScenario for the TitleScreen
+        val titleScenario = launchFragmentInContainer<InputFragment>()
+
+        // Set the NavController property on the fragment
+        titleScenario.onFragment { fragment ->
+            Navigation.setViewNavController(fragment.requireView(), mockNavController)
+        }
+
+        // Verify that performing a click prompts the correct Navigation action
+        onView(withId(R.id.go)).perform(click())
+        verify(mockNavController).navigate(R.id.forecastFragment)
+    }
+}
